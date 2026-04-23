49a6150d9afc376d0c2dda1bfc649e8f5ddbb63c
Gui-Gama <guigamamassela@gmail.com>
2026-04-23 20:57:56 -0300

    aula 23/04/2026

--- _ monitoramento/monitoramento/src/main/java/monitoramento/repository/MedicaoRepository.java
+++ _ monitoramento/monitoramento/src/main/java/monitoramento/repository/MedicaoRepository.java
@@ -1,4 +1,22 @@
 package monitoramento.repository;
 
-public class MedicaoRepository {
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import monitoramento.model.AreaMonitoramento;
+import monitoramento.model.Medicao;
+
+@Repository
+public interface MedicaoRepository extends JpaRepository<Medicao, Long> {
+
+    List<Medicao> findByAreaOrderByDataColetaDesc(AreaMonitoramento area);
+
+    List<Medicao> findByAreaIdOrderByDataColetaDesc(Long areaId);
+
+    List<Medicao> findByDataColetaBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    List<Medicao> findTop10ByAreaOrderByDataColetaDesc(AreaMonitoramento area);
 }
